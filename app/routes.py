f5c124c251dec391b43a0c875d7e25d319f7e905
kiddy <mhudyaramadhana@gmail.com>
2019-12-12 13:28:54 +0700

    Fixed routes.

--- app/routes.py
+++ app/routes.py
@@ -7,8 +7,8 @@ def home():
     return "Hello World"
 
 
-api.add_resource(UserController.WithoutParams, '/users')
-api.add_resource(UserController.WithParams, '/users/<id>')
+api.add_resource(UserController.UsersWithoutParams, '/users')
+api.add_resource(UserController.UsersWithParams, '/users/<id>')
 api.add_resource(UserController.Login, '/login')
 api.add_resource(TodoController.TodoWithoutParams, '/todo')
 api.add_resource(TodoController.TodoWithParams, '/todo/<id>')
